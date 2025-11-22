cdd9da331072197702abbfcc4c8368ef30ec2b94
omdhage04 <omdhage04@gmail.com>
2025-11-22 11:01:16 +0000

    new project started

--- shortmylink/public_html/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('SMTP_USER', '905f1d001@smtp-brevo.com');
-define('SMTP_PASS', 'U6DydkNMVg4HI0aF');
-define('SMTP_FROM_EMAIL', 'omdhage04@gmail.com');
-define('SMTP_FROM_NAME', 'ShortMyLink.in');
-?>
