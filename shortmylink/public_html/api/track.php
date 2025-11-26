df73e240fe0005af9d0d376d1492e8984979964a
OM DHAGE <omdhage04@gmail.com>
2025-11-26 20:46:49 +0530

    Delete shortmylink/public_html directory reuploading due to loading errors

--- shortmylink/public_html/api/track.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Content-Type: application/json");
-require_once __DIR__ . '/../../secure_config/db_connect.php';
-require_once __DIR__ . '/../../core_logic/link_algo.php';
-
-// simple placeholder
-echo json_encode(['short_url'=>'https://shortmylink.in/abcd123']);
-?>
